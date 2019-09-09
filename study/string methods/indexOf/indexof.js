5786c025d40468089130af101ae2028565499bce
Siddharth S <siddharth@techbreakthrough.co.in>
2019-09-09 08:41:13 +0530

    string methods js

---
+++ study/string methods/indexOf/indexof.js
@@ -0,0 +1,6 @@
+function indexofFunction(){
+    var stringName = 'this is siddhu';
+    var stringPosition = stringName.indexOf('siddhu');
+    alert(stringPosition)
+    document.getElementById("index").innerHTML=stringPosition
+}
