20a1f27dd918593a6fdd13d447bc40b39cf462d2
Siddharth S <siddharth@techbreakthrough.co.in>
2019-11-06 12:12:09 +0530

    js study updated

--- study/string methods/indexOf/indexof.js
+++ study/string methods/indexOf/indexof.js
@@ -1,6 +1,6 @@
 function indexofFunction(){
     var stringName = 'this is siddhu';
-    var stringPosition = stringName.indexOf('siddhu');
+    var stringPosition = stringName.indexOf('iddhu');
     alert(stringPosition)
     document.getElementById("index").innerHTML=stringPosition
 }
--- study/try catch and finally/try catch.js
+++ study/try catch and finally/try catch.js
@@ -6,4 +6,9 @@ function tryCatchFunction(){
     alert(err)
     alert(err.message)
   }
+  finally{
+    alert("finally working fine !")
+  }
 }
+
+// eventhough the function stops due to some error the finally completes the required code block
